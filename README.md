b2e0b965924194b634b14524a410757add1b2ab2
sachin-frayne <sachin.frayne@gmail.com>
2023-07-07 14:43:44 +0200

    Add script as is, lots of future changes wanted.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# elfador
-ELasticsearch FAke Data generatOR
+# Elfador - ELasticsearch FAke Data generatOR
+
+Do you need to generate a substantial ammount of Fake data for a use case you are testing in Elasticsearch?
+
+## Usage
+
+```
+python elfador.py
+```
---
+++ elfador.py
@@ -0,0 +1,36 @@
+# Elfador - ELasticsearch FAke Data generatOR
+
+from datetime import datetime
+from elasticsearch import Elasticsearch, helpers
+from random import random, getrandbits, randint
+from ipaddress import IPv4Address
+from essential_generators import DocumentGenerator
+from faker import Faker
+
+bulk_request_size = 10
+number_of_bulk_requests = 10
+random_number_limit = 1000
+index = "index"
+
+elasticsearch_password = ""
+elasticsearch_url = "http://localhost:9200"
+elasticsearch_user = ""
+verify_certs=False
+
+elastic = Elasticsearch([elasticsearch_url],http_auth=(elasticsearch_user, elasticsearch_password),verify_certs=verify_certs)
+gen = DocumentGenerator()
+fake = Faker()
+
+for value in range(0, number_of_bulk_requests):
+    actions = [
+        {
+            "text_field": gen.sentence(),
+            "number_field": randint(0, random_number_limit),
+            "keyword_field": gen.word(),
+            "ip_field": str(IPv4Address(getrandbits(32))),
+            "date_field": fake.date_time_between(start_date='-14d', end_date='now')
+        }
+        for doc in range(bulk_request_size)
+    ]
+
+    response = helpers.bulk(elastic, actions, index=index)
